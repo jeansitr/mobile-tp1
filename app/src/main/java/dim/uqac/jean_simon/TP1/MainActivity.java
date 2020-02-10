e7fb46c848c5cff378841598912f00bf39f4a4c4
Jean-Simon Tremblay <jean-simon_1997@hotmail.com>
2020-02-09 22:32:19 -0500

    :)

---
+++ app/src/main/java/dim/uqac/jean_simon/TP1/MainActivity.java
@@ -0,0 +1,14 @@
+package dim.uqac.jean_simon.TP1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
