48104fd5292cd549f5149f63640391307be1909c
Jean-Simon Tremblay <jean-simon_1997@hotmail.com>
2020-02-16 22:10:32 -0500

    did the thing now is better :)

--- app/src/main/java/dim/uqac/jean_simon/TP1/MainActivity.java
+++ app/src/main/java/dim/uqac/jean_simon/TP1/MainActivity.java
@@ -3,6 +3,10 @@ package dim.uqac.jean_simon.TP1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView valueLastName = (TextView)findViewById(R.id.nomEditTxt);
+        TextView valueName = (TextView)findViewById(R.id.prenomEditTxt);
+        valueLastName.setText(R.string.nom);
+        valueName.setText(R.string.prenom);
+
+        Button button= (Button)findViewById(R.id.updateBtn);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                TextView displayLastName = (TextView)findViewById(R.id.nom);
+                TextView valueLastName = (TextView)findViewById(R.id.nomEditTxt);
+                TextView displayName = (TextView)findViewById(R.id.prenom);
+                TextView valueName = (TextView)findViewById(R.id.prenomEditTxt);
+
+                displayName.setText(valueName.getText());
+                displayLastName.setText(valueLastName.getText());
+            }
+        });
     }
 }
